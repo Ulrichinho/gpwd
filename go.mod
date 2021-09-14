e54763e2bdac141f8d8a318295bc44d7e8d3fbc2
Ulrichinho <grolhier.u@gmail.com>
2021-09-14 12:09:43 +0200

    Update project initialization

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/Ulrichinho/gpwd
+module gpwd
 
 go 1.17
 
--- main.go
+++ main.go
@@ -15,7 +15,7 @@ limitations under the License.
 */
 package main
 
-import "github.com/Ulricinho/gpwd/cmd"
+import "gpwd/cmd"
 
 func main() {
 	cmd.Execute()
